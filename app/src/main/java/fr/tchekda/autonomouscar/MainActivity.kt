5eb03f9598e8486f045d266ed88dcac34846350e
Tchekda (ASUS) <contact@tchekda.fr>
2020-01-15 23:39:26 +0100

    Sending data

--- app/src/main/java/fr/tchekda/autonomouscar/MainActivity.kt
+++ app/src/main/java/fr/tchekda/autonomouscar/MainActivity.kt
@@ -3,7 +3,7 @@ package fr.tchekda.autonomouscar
 import android.os.Bundle
 import android.os.Handler
 import android.os.Message
-import android.widget.TextView
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.ref.WeakReference
@@ -11,6 +11,9 @@ import java.lang.ref.WeakReference
 class MainActivity : AppCompatActivity() {
 
     private var handler: Handler
+    private var socketThread: SocketThread? = null
+    private var data: MutableMap<String, String> = HashMap()
+    private var receivedData: MutableMap<String, String> = HashMap()
 
     init {
         val outerClass = WeakReference(this)
@@ -21,22 +24,62 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        socketThread = SocketThread(true)
+        socketThread!!.start()
+        socketThread!!.setHandler(handler)
 
-        val socketThread = SocketThread()
-        socketThread.start()
-        socketThread.setHandler(handler)
+        switch_dev.setOnCheckedChangeListener { _, isChecked ->
+            socketThread!!.interrupt()
+            socketThread = SocketThread(isChecked)
 
-        button_test.setOnClickListener {
-            Thread {
-                socketThread.sendMessage("Salut mon pote")
-            }.start()
         }
+
+        bar_speed.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                val finalSpeed = progress + 116
+                data["speed"] = finalSpeed.toString()
+
+                sendData()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+        })
+
+
+    }
+
+    fun sendData() {
+        Thread {
+            socketThread!!.sendMessage(data)
+        }.start()
     }
 
     class MyHandler(private val outerClass: WeakReference<MainActivity>) : Handler() {
 
         override fun handleMessage(msg: Message?) {
-            outerClass.get()?.text_message?.text = msg?.obj.toString()
+            val receivePairs = msg?.obj.toString().split("|")
+            val receiveData: MutableMap<String, String> = HashMap<String, String>()
+            receivePairs.forEach {
+                val split = it.split("=")
+                if (split.size == 2) {
+                    receiveData[split[0]] = split[1]
+                }
+            }
+            receiveData.forEach { (key, value) ->
+                when (key) {
+                    "speed" -> {
+                        outerClass.get()?.text_speed_data?.text = value
+                    }
+                }
+            }
+             outerClass.get()?.receivedData = receiveData
         }
     }
 }
--- app/src/main/java/fr/tchekda/autonomouscar/SocketThread.kt
+++ app/src/main/java/fr/tchekda/autonomouscar/SocketThread.kt
@@ -1,25 +1,34 @@
 package fr.tchekda.autonomouscar
 
 import android.os.Handler
+import android.util.Log
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.io.PrintWriter
+import java.net.ConnectException
 import java.net.Socket
 
-class SocketThread:Thread() {
+class SocketThread(private val dev: Boolean = false) : Thread() {
 
     private lateinit var handler: Handler
-    var writer : PrintWriter? = null
+    var writer: PrintWriter? = null
 
     override fun run() {
         super.run()
-        val socket = Socket("192.168.43.98", 2020)
+        val address = if (dev) "192.168.0.48" else "192.168.43.98"
+        val socket: Socket
+        try {
+            socket = Socket(address, 2020)
+        }catch (e: ConnectException) {
+            e.printStackTrace()
+            return
+        }
         val input = socket.getInputStream()
         val reader = BufferedReader(InputStreamReader(input))
         var text: String
         val output = socket.getOutputStream()
         writer = PrintWriter(output, true)
-        while (true){
+        while (true) {
             text = reader.readLine()
             handler.sendMessage(handler.obtainMessage(0, text))
         }
@@ -29,11 +38,14 @@ class SocketThread:Thread() {
         this.handler = handler
     }
 
-    fun sendMessage(string: String) {
-        writer?.println(string)
+    fun sendMessage(data: Map<String, String>) {
+        var message = ""
+        data.forEach { (key, value) ->
+            message += "$key=$value|"
+        }
+        writer?.println(message)
     }
 
     override fun destroy() {
-
     }
 }
