17537bf1fcde4f2bc841c92e89fd78f39ce6177a
Tchekda (ASUS) <contact@tchekda.fr>
2020-01-17 19:00:41 +0100

    Car Speed Control

--- app/src/main/java/fr/tchekda/autonomouscar/MainActivity.kt
+++ app/src/main/java/fr/tchekda/autonomouscar/MainActivity.kt
@@ -24,21 +24,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        socketThread = SocketThread(false)
-        socketThread!!.start()
-        socketThread!!.setHandler(handler)
+        initSocket(true)
 
         switch_dev.setOnCheckedChangeListener { _, isChecked ->
-            socketThread!!.interrupt()
-            socketThread = SocketThread(isChecked)
-
+            socketThread?.disconnect()
+            socketThread?.interrupt()
+            initSocket(isChecked)
         }
 
         bar_speed.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 val finalSpeed = progress + 116
                 data["speed"] = finalSpeed.toString()
-
+                data["keep"] = "1"
                 sendData()
             }
 
@@ -55,11 +53,24 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        socketThread?.disconnect()
+    }
+
+    fun initSocket(dev: Boolean){
+        socketThread = SocketThread(dev)
+        socketThread?.start()
+        socketThread?.setHandler(handler)
+    }
+
     fun sendData() {
         Thread {
             socketThread!!.sendMessage(data)
         }.start()
     }
+
+
 
     class MyHandler(private val outerClass: WeakReference<MainActivity>) : Handler() {
 
@@ -77,9 +88,18 @@ class MainActivity : AppCompatActivity() {
                     "speed" -> {
                         outerClass.get()?.text_speed_data?.text = value
                     }
+                    "connect" -> {
+                        if (value.toInt() == 1) {
+                            outerClass.get()?.text_connect_data?.text =
+                                outerClass.get()?.getString(R.string.connection_on)
+                        } else {
+                            outerClass.get()?.text_connect_data?.text =
+                                outerClass.get()?.getString(R.string.connection_off)
+                        }
+                    }
                 }
             }
-             outerClass.get()?.receivedData = receiveData
+            outerClass.get()?.receivedData = receiveData
         }
     }
 }
--- app/src/main/java/fr/tchekda/autonomouscar/SocketThread.kt
+++ app/src/main/java/fr/tchekda/autonomouscar/SocketThread.kt
@@ -5,25 +5,46 @@ import android.util.Log
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.io.PrintWriter
+import java.lang.IllegalStateException
 import java.net.ConnectException
+import java.net.NoRouteToHostException
 import java.net.Socket
 import java.net.SocketException
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class SocketThread(private val dev: Boolean = false) : Thread() {
 
     private lateinit var handler: Handler
+    private lateinit var socket: Socket
+    private lateinit var executor: ExecutorService
+
     var writer: PrintWriter? = null
 
     override fun run() {
         super.run()
-        val address = if (dev) "192.168.43.101" else "192.168.43.98"
-        val socket: Socket
+        val address = if (dev) "192.168.0.48" else "192.168.43.98"
+        val port = 2020
+        Log.i("Socket", "Initializing $address:$port")
+        handler.sendMessage(handler.obtainMessage(0, "connect=0|"))
         try {
-            socket = Socket(address, 2020)
+            socket = Socket(address, port)
         }catch (e: ConnectException) {
-            e.printStackTrace()
+            Log.i("Socket", "Connection to $address:$port failed")
+            //e.printStackTrace()
+            handler.sendMessage(handler.obtainMessage(0, "connect=0|"))
+            return
+        }catch (e: NoRouteToHostException){
+            Log.i("Socket", "Can't find $address, Host unreachable")
+            //e.printStackTrace()
+            handler.sendMessage(handler.obtainMessage(0, "connect=0|"))
             return
         }
+        Log.i("Socket", "Connection to $address:$port succeeded")
+        handler.sendMessage(handler.obtainMessage(0, "connect=1|"))
+
+        executor = Executors.newFixedThreadPool(1)
+
         val input = socket.getInputStream()
         val reader = BufferedReader(InputStreamReader(input))
         var text: String
@@ -32,27 +53,59 @@ class SocketThread(private val dev: Boolean = false) : Thread() {
         while (true) {
             try {
                 text = reader.readLine()
-                Log.i("Socket", text)
+                Log.i("Socket","Received : $text")
                 handler.sendMessage(handler.obtainMessage(0, text))
             }catch (e: SocketException){
+                Log.i("Socket", "Connection to $address:$port has been reset")
+                e.printStackTrace()
+                break
+            }catch (e: IllegalStateException){
+                Log.i("Socket", "Connection to $address:$port has crashed")
                 e.printStackTrace()
                 break
             }
         }
+
+        disconnect()
+
     }
 
     fun setHandler(handler: Handler) {
         this.handler = handler
     }
 
-    fun sendMessage(data: Map<String, String>) {
-        var message = ""
-        data.forEach { (key, value) ->
-            message += "$key=$value|"
+    fun disconnect(){
+        if (::socket.isInitialized){
+            if (!socket.isClosed) {
+                executor.shutdownNow()
+                while (!executor.isTerminated) {
+                }
+                socket.close()
+                Log.i("Socket", "Socket disconnected")
+                handler.sendMessage(handler.obtainMessage(0, "connect=0|"))
+            }
         }
-        writer?.println(message)
+    }
+
+    fun sendMessage(data: Map<String, String>) {
+        if (::socket.isInitialized) {
+            if (!socket.isClosed) {
+                val worker = Runnable {
+                    var message = ""
+                    data.forEach { (key, value) ->
+                        message += "$key=$value|"
+                    }
+                    writer?.println(message)
+                }
+                executor.execute(worker)
+            }
+        }
+
     }
 
     override fun destroy() {
+        Log.i("Socket", "Terminating Socket Thread")
+        disconnect()
+        handler.sendMessage(handler.obtainMessage(0, "connect=0|"))
     }
 }
